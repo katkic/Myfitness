0b43c266e04fe1d5045c56a30213606fe0134546
daisaku kato <50142017+katkic@users.noreply.github.com>
2020-05-19 11:06:02 +0900

    fix: 落ちていたテストコードを修正

--- spec/models/workout_spec.rb
+++ spec/models/workout_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe 'workoutのクラスメソッド', type: :model do
+RSpec.describe 'workoutのプライベートメソッド', type: :model do
+  let(:instance) { Workout.new }
   let!(:user) { create(:user) }
   let!(:exercise) { create(:exercise, name: 'ベンチプレス', user: user) }
   let!(:exercise2) { create(:exercise, name: 'スクワット', user: user) }
@@ -15,50 +16,58 @@ RSpec.describe 'workoutのクラスメソッド', type: :model do
     @exercise_logs = ExerciseLog.limit(3)
   end
 
-  describe 'トレーニングのセット数を取得 Workout.get_set' do
-    it '行ったトレーニングのセット数を返すこと' do
-      expect(Workout.get_set(@exercise_logs)).to eq 3
-    end
-  end
+  describe 'トレーニングの最大weightを取得 get_max_weight' do
+    subject { instance.send(:get_max_weight, @exercise_logs) }
 
-  describe 'トレーニングの最大weightを取得 Workout.get_max_weight' do
     it '行ったトレーニングの最大weightを返すこと' do
-      expect(Workout.get_max_weight(@exercise_logs)).to eq 62.5
+      expect(subject).to eq 62.5
     end
   end
 
-  describe 'トレーニングのトータル重量を取得 Workout.get_max_weight' do
+  describe 'トレーニングのトータル重量を取得 get_max_weight' do
+    subject { instance.send(:get_total_weight, @exercise_logs) }
+
     it '行ったトレーニングのトータル重量を返すこと' do
-      expect(Workout.get_total_weight(@exercise_logs)).to eq 1800
+      expect(subject).to eq 1800
     end
   end
 
-  describe 'トレーニングの1RMを取得 Workout.get_one_rm_max' do
-    context 'トレーニングが1repの場合' do
+  describe 'トレーニングの1RMを取得 get_one_rm_max' do
+    context 'トレーニングが1rep(62.5kg)の場合' do
       before do
         create(:exercise_log, set: 1, weight: 60.0, rep: 1, workout: bench_press)
         create(:exercise_log, set: 2, weight: 62.5, rep: 1, workout: bench_press)
         @exercise_logs = ExerciseLog.offset(3)
       end
+      subject { instance.send(:get_max_one_rm, @exercise_logs) }
 
-      it 'その重量を返すこと' do
-        expect(Workout.get_one_rm_max(@exercise_logs)).to eq 62.5
+      it 'その重量(62.5kg)を返すこと' do
+        expect(subject).to eq 62.5
       end
     end
 
-    context 'トレーニングが複数repの場合' do
+    context 'トレーニングが複数repの場合(スクワットorデッドリフト以外)' do
+      before do
+        create(:exercise_log, set: 1, weight: 62.5, rep: 10, workout: squat)
+        create(:exercise_log, set: 2, weight: 60.0, rep: 10, workout: squat)
+      end
+      subject { instance.send(:get_max_one_rm, @exercise_logs) }
+
+      it '補正係数40.0で計算' do
+        expect(subject).to eq 78.1
+      end
+    end
+
+    context 'トレーニングが複数repの場合(スクワットorデッドリフト)' do
       before do
         create(:exercise_log, set: 1, weight: 62.5, rep: 10, workout: squat)
         create(:exercise_log, set: 2, weight: 60.0, rep: 10, workout: squat)
         @squat_logs = ExerciseLog.offset(3)
       end
+      subject { instance.send(:get_max_one_rm, @squat_logs) }
 
-      it 'ベンチプレスのとき、補正係数40.0で計算' do
-        expect(Workout.get_one_rm_max(@exercise_logs)).to eq 78.1
-      end
-
-      it 'スクワットorデッドリフトの時、補正係数33.3で計算' do
-        expect(Workout.get_one_rm_max(@squat_logs)).to eq 81.2
+      it '補正係数33.3で計算' do
+        expect(subject).to eq 81.2
       end
     end
   end
