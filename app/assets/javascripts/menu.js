22dcc2e6575fefa04939f1c812a1fd9eb7f7619c
daisaku kato <50142017+katkic@users.noreply.github.com>
2020-05-23 11:56:47 +0900

    fix: メニュー作成画面のエラーメッセージ色を修正
    エラーメッセージが赤色になるように修正

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -26,10 +26,12 @@ $(document).on('turbolinks:load', function () {
     } else if (!menuName) {
       setErrorMsg(nameAlert, MSG_EMPTY);
       clearErrorMsg(checkBoxAlert);
+      nameAlert.addClass('has-error-msg');
       return false;
     } else if (checkCount === 0) {
       clearErrorMsg(nameAlert);
       setErrorMsg(checkBoxAlert, MSG_CHECK_BOX);
+      checkBoxAlert.addClass('has-error-msg');
       return false;
     }
   });
