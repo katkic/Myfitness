16ce941f1e41dfb036f9497cda93b433880a222e
daisaku kato <50142017+katkic@users.noreply.github.com>
2020-05-23 10:42:39 +0900

    update: トレーニング記録フォームのバリデーションを変更
    フロント側ではなく、バックエンド側で処理することとする

---
+++ app/assets/javascripts/menu.js
@@ -0,0 +1,36 @@
+$(document).on('turbolinks:load', function () {
+  const MSG_EMPTY = 'メニュー名を入力してください';
+  const MSG_CHECK_BOX = 'トレーニング種目を選択してください';
+
+  function setErrorMsg(obj, msg) {
+    obj.text(msg);
+  }
+
+  function clearErrorMsg(obj) {
+    obj.text('');
+  }
+
+  $('#menu-form').submit(function () {
+    const menuName = $('#menu_name').val();
+    const checkCount = $('.check-box :checked').length;
+    const nameAlert = $('.form-name-alert');
+    const checkBoxAlert = $('.form-chackbox-alert');
+    const tabContent = $('.tab-content');
+
+    if (!menuName && checkCount === 0) {
+      setErrorMsg(nameAlert, MSG_EMPTY);
+      setErrorMsg(checkBoxAlert, MSG_CHECK_BOX);
+      nameAlert.addClass('has-error-msg');
+      checkBoxAlert.addClass('has-error-msg');
+      return false;
+    } else if (!menuName) {
+      setErrorMsg(nameAlert, MSG_EMPTY);
+      clearErrorMsg(checkBoxAlert);
+      return false;
+    } else if (checkCount === 0) {
+      clearErrorMsg(nameAlert);
+      setErrorMsg(checkBoxAlert, MSG_CHECK_BOX);
+      return false;
+    }
+  });
+});
